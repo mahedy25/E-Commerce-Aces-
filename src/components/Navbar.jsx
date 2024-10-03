2b437eb92e5060452bac58ec654e16c70dbd36ba
mahedy25 <mahedyhasan873@gmail.com>
2024-10-03 17:16:06 +0600

    same as last

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
         {/*--------sidebar menu---------*/}
         <div className={`fixed top-0 right-0 w-64 h-full bg-cyan-500 p-5 transform ${
           visible ? 'translate-x-0' : 'translate-x-full'
-        } transition-transform duration-5000 ease-in z-50`}>
+        } transition-transform duration-300 ease-in z-50`}>
             <div className='flex flex-col text-black font-sans font-semibold'>
 
             <div className="flex justify-end items-center group relative">
